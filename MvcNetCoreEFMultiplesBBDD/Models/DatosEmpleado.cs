a323041ebedebc8e4ae024f7150adf635c28c14b
Jorgerdzz <asilgalan083@gmail.com>
2026-02-13 11:51:41 +0100

    Add project files.

---
+++ MvcNetCoreEFMultiplesBBDD/Models/DatosEmpleado.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcNetCoreEFMultiplesBBDD.Models
+{
+    [Table("V_EMPLEADOS")]
+    public class DatosEmpleado
+    {
+        [Key]
+        [Column("IDEMPLEADO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public string Apellido { get; set; }
+        [Column("OFICIO")]
+        public string Oficio { get; set; }
+        [Column("SALARIO")]
+        public int Salario { get; set; }
+        [Column("DEPT_NO")]
+        public int IdDepartamento { get; set; }
+        [Column("DNOMBRE")]
+        public string NombreDepartamento { get; set; }
+        [Column("LOC")]
+        public string Localidad { get; set; }
+    }
+}
